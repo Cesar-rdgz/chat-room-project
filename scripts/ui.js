6d5af083543b19a643e2e5e0423c424e9d326cb7
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2021-04-07 20:53:48 -0500

    fixing date bug

--- scripts/ui.js
+++ scripts/ui.js
@@ -9,10 +9,7 @@ class ChatUI {
         this.list.innerHTML = '';
     }
     render(data){
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            { addSuffix: true }
-        )
+        const when = data.created_at.toDate();
         const html = `
             <li class="list-group-item">
                 <span class="username">${data.username}</span>
